a7b9a278b4aada3df284a2c7bc9dfc1fcf99d1a7
Ilya Yaskevych <ogurethcs@gmail.com>
2022-11-13 18:48:50 +0200

    Changed h1

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { products } from './data/products';
 function App() {
    return (
     <div className='containet mx-auto max-w-2xl pt-5'>
-      <h1>Lera Loh</h1>
+      <h1>Lera Loasdh</h1>
       { products.map(product => <Product product={product} key={product.id}/>)}
     </div>
    )
